a3cf8a624e73ce9d7eb7c48ae2e994ba8009dd8c
Hicaro Andre <hicaroandre@hotmail.com>
2025-05-15 21:29:33 -0300

    feat: menu mobile and plans v.01

--- src/assets/js/plans.js
+++ src/assets/js/plans.js
@@ -1,29 +1,5 @@
 // TODO: script do carrossel dos cards 
-const wrapper = document.querySelector(".plans__carousel-wrapper");
-const cards = document.querySelectorAll(".plans__free");
-const arrowRight = document.querySelector(".arrow-dir");
-const arrowLeft = document.querySelector(".arrow-esq");
 
-let currentIndex = 0;
-
-function updateCarousel() {
-  const offset = currentIndex * -100;
-  wrapper.style.transform = `translateX(${offset}%)`;
-}
-
-arrowRight.addEventListener("click", () => {
-  if (currentIndex < cards.length - 1) {
-    currentIndex++;
-    updateCarousel();
-  }
-});
-
-arrowLeft.addEventListener("click", () => {
-  if (currentIndex > 0) {
-    currentIndex--;
-    updateCarousel();
-  }
-});
 
 // TODO: scripts dos ícones acima dos cards do carrossel 
 const indicators = document.querySelectorAll(".plans__buttons-select img");
