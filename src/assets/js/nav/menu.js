3f8a98b36d37120fe44d515f7e7b602ce35bab55
Hicaro Andre <hicaroandre@hotmail.com>
2025-05-16 15:42:08 -0300

    feat: update hero

--- src/assets/js/nav/menu.js
+++ src/assets/js/nav/menu.js
@@ -6,8 +6,50 @@ toggleBtn.addEventListener('click', () => {
   nav.classList.toggle('open');
 });
 
+//TODO: navegação ativa dos links Mobile
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const nav = document.querySelector(".nav");
+    const closeBtn = document.getElementById("closeMenu");
+    const links = document.querySelectorAll(".nav a");
+    const sections = document.querySelectorAll("section[id]");
 
-//TODO: navegação ativa dos links
+    // Abre o menu
+    menuToggle.addEventListener("click", () => {
+      nav.classList.add("open");
+    });
+
+    // Fecha o menu e roda o X
+    closeBtn.addEventListener("click", () => {
+      nav.classList.remove("open");
+      closeBtn.classList.add("rotate");
+      setTimeout(() => closeBtn.classList.remove("rotate"), 300);
+    });
+
+    // Scroll Spy
+    window.addEventListener("scroll", () => {
+      let current = "";
+
+      sections.forEach(section => {
+        const sectionTop = section.offsetTop - 100;
+        const sectionHeight = section.clientHeight;
+
+        if (pageYOffset >= sectionTop && pageYOffset < sectionTop + sectionHeight) {
+          current = section.getAttribute("id");
+        }
+      });
+
+      links.forEach(link => {
+        link.classList.remove("active");
+        if (link.getAttribute("href").includes(current)) {
+          link.classList.add("active");
+        }
+      });
+    });
+  });
+
+  
+//TODO: navegação ativa dos links Desktop
   const navLinks = document.querySelectorAll('.nav a');
 
   //? Clique: marcar link ativo
