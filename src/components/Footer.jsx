6fffae3171ae3bc0f0eff2293c4fee6050721cea
Hicaro Andre <hicaroandre@hotmail.com>
2025-06-14 18:05:42 -0300

    feat: footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,28 +1,27 @@
-import React from 'react';
-import '../styles/Footer.css'
-import logoFooter from '../assets/images/footer/logo-footer.svg';
-import faceIcon from '../assets/images/footer/face.svg';
-import instaIcon from '../assets/images/footer/insta.svg';
-import youtubeIcon from '../assets/images/footer/youtube.svg';
+import React from "react";
+import "../styles/Footer.css";
+import logoFooter from "../assets/images/footer/logo-footer.svg";
+import faceIcon from "../assets/images/footer/face.svg";
+import instaIcon from "../assets/images/footer/insta.svg";
+import youtubeIcon from "../assets/images/footer/youtube.svg";
 
 //* componente filho
-function Footer (){
-
-    const navLinks = [
-    { id: 1, text: 'Home', href: '/' },
-    { id: 2, text: 'Sobre', href: '/about' },
-    { id: 3, text: 'Planos', href: '/plans' },
-    { id: 4, text: 'Blog', href: '/blog' },
+function Footer() {
+  const navLinks = [
+    { id: 1, text: "Home", href: "/" },
+    { id: 2, text: "Sobre", href: "/about" },
+    { id: 3, text: "Planos", href: "/plans" },
+    { id: 4, text: "Blog", href: "/blog" },
   ];
 
   const socialIcons = [
-    { id: 1, icon: faceIcon, alt: 'Facebook', className: 'svg-icon1' },
-    { id: 2, icon: instaIcon, alt: 'Instagram', className: 'svg-icon3' },
-    { id: 3, icon: youtubeIcon, alt: 'YouTube', className: 'svg-icon4' }
+    { id: 1, icon: faceIcon, alt: "Facebook", className: "svg-icon1" },
+    { id: 2, icon: instaIcon, alt: "Instagram", className: "svg-icon3" },
+    { id: 3, icon: youtubeIcon, alt: "YouTube", className: "svg-icon4" },
   ];
 
-  return(
-     <footer>
+  return (
+    <footer>
       <div className="footer__content">
         <img src={logoFooter} alt="Logo" />
 
@@ -36,19 +35,18 @@ function Footer (){
             />
           ))}
         </div>
-      </div>
-
-      <div className="footer__nav">
-        <ul>
-          {navLinks.map((link) => (
-            <li key={link.id}>
-              <a href={link.href}>{link.text}</a>
-            </li>
-          ))}
-        </ul>
+        <div className="footer__nav">
+          <ul>
+            {navLinks.map((link) => (
+              <li key={link.id}>
+                <a href={link.href}>{link.text}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </footer>
-  )
+  );
 }
 
 export default Footer;
