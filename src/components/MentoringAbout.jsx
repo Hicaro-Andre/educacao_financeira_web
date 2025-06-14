ae147771c216616354818fbfa481c02ce11b57f0
Hicaro Andre <hicaroandre@hotmail.com>
2025-06-14 14:35:42 -0300

    feat: blog v2

--- src/components/MentoringAbout.jsx
+++ src/components/MentoringAbout.jsx
@@ -1,16 +1,40 @@
 import React from 'react';
 import '../styles/MentoringAbout.css';
 import mentoringImage from '../assets/images/hero_about/heroabout-image.png'; // ajuste o caminho conforme sua estrutura de pastas
-
+import mentoringImageSmall from "../assets/images/hero_about/heroabout-image.png"
 const MentoringAbout = () => {
-  return (
+ return (
     <section className="mentoring-about">
       <div className="overlay-image" />
-      <h3>Sobre a Mentoria</h3>
-      <img src={mentoringImage} alt="Sobre a Mentoria" />
-      <button className="plans-button">
-        Conheça nossos planos
-      </button>
+      
+      {/* Container do texto + botão (esquerda) */}
+      <div className="text-content">
+        <h3>Sobre a Mentoria</h3>
+        <p>
+          Conheça mais sobre a Mentora, sua Visão, Missão e Valores, e como ela transforma a vida financeira e o casamento de casais cristãos.
+        </p>
+        <button 
+          className="plans-button" 
+          onClick={() => (window.location.href = "/plans")}
+          aria-label="Encontre seu plano"
+        >
+          Conheça nossos planos
+        </button>
+      </div>
+
+      {/* Container da imagem (direita) */}
+      <picture>
+        <source media="(min-width: 1200px)" srcSet={mentoringImageSmall} />
+        <source media="(min-width: 800px)" srcSet={mentoringImageSmall} />
+        <img src={mentoringImageSmall} alt="Sobre a Mentoria" />
+         <button 
+          className="plans-button-mob" 
+          onClick={() => (window.location.href = "/plans")}
+          aria-label="Encontre seu plano"
+        >
+          Conheça nossos planos
+        </button>
+      </picture>
     </section>
   );
 };
