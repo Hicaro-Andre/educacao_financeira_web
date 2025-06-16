cbcc02fd06d3593b62c01ff1b9dc850692e9277f
Hicaro Andre <hicaroandre@hotmail.com>
2025-06-16 17:16:03 -0300

    feat: update plans

--- src/components/PlansSection.jsx
+++ src/components/PlansSection.jsx
@@ -132,24 +132,25 @@ function PlansSection() {
               <p className="duration">{plan.duration}</p>
               <ul>
                 {plan.items.map((item, i) => (
-                  <li key={i}>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="20"
-                      height="20"
-                      viewBox="0 0 21 20"
-                      fill="none"
-                    >
-                      <path
-                        d="M17.1666 5L7.99992 14.1667L3.83325 10"
-                        stroke="#22C55E"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      />
-                    </svg>
-                    {item}
-                  </li>
+                   <li key={index}>
+                  <svg
+                    className="plan-item-icon"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 21 20"
+                    fill="none"
+                  >
+                    <path
+                      d="M17.1666 5L7.99992 14.1667L3.83325 10"
+                      stroke="#22C55E"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                  {item}
+                </li>
                 ))}
               </ul>
               <button
