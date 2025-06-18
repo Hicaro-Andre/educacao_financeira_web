44267ac810b78fdf85998068d6d1a13c7d0f3c6d
Hicaro Andre <hicaroandre@hotmail.com>
2025-06-18 19:29:27 -0300

    feat: phone giseli message test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -41,7 +41,7 @@ function Home() {
               const message =
                 "Olá, estou buscando mais informações sobre as mentorias";
               window.open(
-                `https://wa.me/48984927741?text=${encodeURIComponent(message)}`,
+                `https://wa.me/+5548984927741?text=${encodeURIComponent(message)}`,
                 "_blank"
               );
             }}
